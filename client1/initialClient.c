2ef0c176a476424a609313910e9d68c6ae7c59ad
emalppro@gmail.com <emalppro@gmail.com>
2017-11-15 11:47:56 +1100

    Shits not working but i'm too lazy to fix it

--- client1/initialClient.c
+++ client1/initialClient.c
@@ -3,9 +3,9 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
-#include "include/startClient.h"
+#include "include/initialClient.h"
 
-int serverPort = atoi(9000);
+int serverPort = 9000;
 struct sockaddr_in server_address;
 struct hostent *server;
 
@@ -16,16 +16,16 @@ int startClient(){
 // create a client socket
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	
-	server = gethostbyname("localhost");
+	server = gethostbyname("127.0.0.1");
 	bzero((char *)&server_address, sizeof(server_address));
 	
 	server_address.sin_family = AF_INET;
 
 	bcopy((char *)&server->h_addr ,(char *)&server_address.sin_addr.s_addr, server->h_length );
 
-	server_address.sin_port = serverPort;	
+	server_address.sin_port = htons(serverPort);	
 
-	int connected = connect(clientSocket, &server_address, server->h_length);
+	int connected = connect(clientSocket, &server_address, sizeof(server_address));
 
 	if(connected <0){
 		perror("Cannot connect to server");
@@ -35,9 +35,8 @@ int startClient(){
 	printf("Successfully connected to server.\n Ready to start RPC between the computers");
 
 	// this is only for testing purpose.
-	char buf[1024];
-	buf = "Emalp is best!";
-	write(clientsocket, &buf, sizeof(buf));
+	char *buf = "Emalp is the best";
+	write(clientSocket, buf, sizeof(buf));
 	
 	close(clientSocket);
 return 0;
