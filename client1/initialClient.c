083409e021cdc23cf18915e069f77a6f717aefc6
emalppro@gmail.com <emalppro@gmail.com>
2017-11-15 11:17:56 +1100

    Client made

--- client1/initialClient.c
+++ client1/initialClient.c
@@ -5,11 +5,34 @@
 #include<netdb.h>
 
 
-int main(){
+int serverPort = atoi(9000);
+struct sockaddr_in server_address;
+struct hostent *server;
 
+int clientSocket;
+
+int startClient(){
+
+// create a client socket
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	
+	server = gethostbyname("localhost");
+	bzero((char *)&server_address, sizeof(server_address));
+	
+	server_address.sin_family = AF_INET;
 
+	bcopy((char *)&server->h_addr ,(char *)&server_address.sin_addr.s_addr, server->h_length );
 
+	server_address.sin_port = serverPort;	
+
+	int connected = connect(clientSocket, &server_address, server->h_length);
+
+	if(connected <0){
+		perror("Cannot connect to server");
+		exit(1);
+	}
+
+	printf("Successfully connected to server.\n Ready to start RPC between the computers");
 
 return 0;
 }
