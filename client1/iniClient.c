2639f72cdfc6d0e5d423f1caa3642a89ae149538
emalppro@gmail.com <emalppro@gmail.com>
2017-11-17 19:46:42 +1100

    The initial baby client worksssgit status!

--- client1/iniClient.c
+++ client1/iniClient.c
@@ -5,18 +5,45 @@
 #include<netdb.h>
 #include<stdlib.h>
 
+const int port = 9000;
 
 int clientSocket;
 
+struct sockaddr_in server_address;
+struct hostent *server_ip_details;
+
 void startClient(){
-	printf("Hello world\n");
+	printf("client started...\n");
+	
+	bzero((char *)&server_address , sizeof(server_address));
+
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);	
+
+	server_ip_details = gethostbyname("localhost");
+
+	
+	bcopy(server_ip_details->h_addr, (char *)&server_address.sin_addr.s_addr, sizeof(server_ip_details->h_length));
+	
+
 
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
 	if(clientSocket > 0){
 
-		printf("Client socket created!");
+		printf("Client socket created!\n");
+		int connected = connect(clientSocket,(struct sockaddr_in *)&server_address, sizeof(server_address) );	
 		
+		if(connected != 0){
+			perror("Client can't connect to server\n");
+			exit(1);
+		}
+
+		char *buf = "Emalp";
+		write(clientSocket, buf, sizeof(buf));
+		printf("Message sent to server\n");
+		close(clientSocket);
 	}
 
 }
