30e620e4560bdb836bebcfd83dc7b4fcefa9226b
emalppro@gmail.com <emalppro@gmail.com>
2017-11-17 18:55:58 +1100

    Client made a little bit better

--- client1/iniClient.c
+++ client1/iniClient.c
@@ -1,9 +1,29 @@
 #include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<stdlib.h>
 
-int main(){
+
+int clientSocket;
+
+void startClient(){
 	printf("Hello world\n");
 
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(clientSocket > 0){
+
+		printf("Client socket created!");
+		
+	}
+
+}
+
+int main(){
+
+	startClient();
 
 return 0;
-
 }
